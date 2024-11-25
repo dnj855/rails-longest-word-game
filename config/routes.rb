05003b6324edbd80b68451fd04632df70b3aa2e0
Cedric Lang-Roth <dnj855@yahoo.com>
2024-11-25 17:25:39 +0100

    Some small corrections

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "games#new"
-  get "/new", to: 'games#new'
-  post "/score", to: "games#score"
+  get "new", to: 'games#new'
+  post "score", to: "games#score"
 end
